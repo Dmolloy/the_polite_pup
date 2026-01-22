c014f65d7b4e67aeff6d1c6bba589ba13561fd48
Dmolloy <declanjmolloy@gmail.com>
2026-01-22 12:17:53 +0100

    add models and admin set up

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TrainingSession, Booking
 
-# Register your models here.
+admin.site.register(TrainingSession)
+admin.site.register(Booking)
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class TrainingSession(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    session = models.ForeignKey(
+        TrainingSession,
+        on_delete=models.CASCADE
+    )
+    date = models.DateField()
+    time = models.TimeField()
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.session}"
